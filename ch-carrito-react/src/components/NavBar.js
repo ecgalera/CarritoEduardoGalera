a3bf4c5429642c60db0383fc65b90bdea054188d
Eduardo Galera <galera.eduardo@gmail.com>
2021-12-09 14:41:09 -0300

    Segundo Commit NavBarcorregido

--- ch-carrito-react/src/components/NavBar.js
+++ ch-carrito-react/src/components/NavBar.js
@@ -1,6 +1,17 @@
 import React from "react";
 
+// const navToggle = document.querySelector(".nav-toggle")
+// const navMenu = document.querySelector(".nav-menu")
 
+// navToggle.addEventListener("click", () => {
+//     navMenu.classList.toggle("nav-menu_visible")
+
+//     if (navMenu.classList.contains("nav-menu_visible")) {
+//         navToggle.setAttribute("aria-label", "Cerrar menú");
+//     } else {
+//         navToggle.setAttribute("aria-label", "Abrir menú")
+//     }
+// })
 
 const NavBar = ()=>{
     return (
@@ -8,15 +19,41 @@ const NavBar = ()=>{
               
         <header className = "header"> 
             <nav className = "nav">
-                <a href = " " className="logo nav-link">Logo</a>
+                <a href = "https://reactjs.org" className="logo nav-link">Logo</a>
                 <button className="nav-toggle">
-                Responsive
+                    <img src="/img/menuAmbur.png" width={30} className="nav-menu-img"/>
                 </button>
                  <ul className = "nav-menu">
-                    <li className ="nav-menu-item nav-link">Contacto</li> 
-                    <li className ="nav-menu-item nav-link">Clientes</li>
-                    <li className ="nav-menu-item nav-link">roductos</li>
-                    <li className ="nav-menu-item nav-link">Servicios</li>
+                    <li className ="nav-menu-item nav-link">
+                        <a className="App-link nav-menu-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer">Contacto
+                        </a>
+                    </li> 
+                    <li className ="nav-menu-item nav-link"> 
+                        <a className="nav-menu-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer">Clientes
+                        </a>
+                   
+                    </li>
+                    <li className ="nav-menu-item nav-link">
+                        <a
+                        className="nav-menu-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer">Productos 
+                        </a>
+                    </li>
+                    <li className ="nav-menu-item nav-link">
+                        <a className="nav-menu-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer">Servicios 
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </header>
